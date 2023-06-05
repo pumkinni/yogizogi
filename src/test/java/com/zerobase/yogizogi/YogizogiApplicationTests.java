0755361a45bff212085ea25f4a4b1dfee7d3e5a4
pumkinni <cindy4766@naver.com>
2023-06-05 10:16:38 +0900

    Initial commit

---
+++ src/test/java/com/zerobase/yogizogi/YogizogiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.yogizogi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogizogiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
